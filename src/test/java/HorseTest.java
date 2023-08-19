a49a3cc18272eeb901eb1e94b264a60732861107
andrew <avcopper@mail.ru>
2023-08-19 23:01:56 +0600

    tests

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -11,38 +11,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HorseTest {
     @Test
     public void firstParamConstructorNullException() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 1,2));
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 1, 2));
         assertEquals("Name cannot be null.", exception.getMessage());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "\n", "\t"})
     public void firstParamConstructorEmptyException(String name) {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse(name, 1,2));
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse(name, 1, 2));
         assertEquals("Name cannot be blank.", exception.getMessage());
     }
 
     @Test
     public void secondParamConstructorNegativeException() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Test", -1,2));
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Test", -1, 2));
         assertEquals("Speed cannot be negative.", exception.getMessage());
     }
 
     @Test
     public void thirdParamConstructorNegativeException() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Test", 1,-2));
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Test", 1, -2));
         assertEquals("Distance cannot be negative.", exception.getMessage());
     }
 
     @Test
     public void getName() {
-        Horse horse = new Horse("Test", 1,2);
+        Horse horse = new Horse("Test", 1, 2);
         assertEquals("Test", horse.getName());
     }
 
     @Test
     public void getNameByReflection() throws NoSuchFieldException, IllegalAccessException {
-        Horse horse = new Horse("Test", 1,2);
+        Horse horse = new Horse("Test", 1, 2);
         Field nameField = horse.getClass().getDeclaredField("name");
         nameField.setAccessible(true);
         String name = (String) nameField.get(horse);
@@ -51,13 +51,13 @@ public class HorseTest {
 
     @Test
     public void getSpeed() {
-        Horse horse = new Horse("Test", 1,2);
+        Horse horse = new Horse("Test", 1, 2);
         assertEquals(1, horse.getSpeed());
     }
 
     @Test
     public void getSpeedByReflection() throws NoSuchFieldException, IllegalAccessException {
-        Horse horse = new Horse("Test", 1,2);
+        Horse horse = new Horse("Test", 1, 2);
         Field speedField = horse.getClass().getDeclaredField("speed");
         speedField.setAccessible(true);
         double speed = (double) speedField.get(horse);
@@ -66,13 +66,13 @@ public class HorseTest {
 
     @Test
     public void getDistance() {
-        Horse horse = new Horse("Test", 1,2);
+        Horse horse = new Horse("Test", 1, 2);
         assertEquals(2, horse.getDistance());
     }
 
     @Test
     public void getDistanceByReflection() throws NoSuchFieldException, IllegalAccessException {
-        Horse horse = new Horse("Test", 1,2);
+        Horse horse = new Horse("Test", 1, 2);
         Field distanceField = horse.getClass().getDeclaredField("distance");
         distanceField.setAccessible(true);
         double distance = (double) distanceField.get(horse);
